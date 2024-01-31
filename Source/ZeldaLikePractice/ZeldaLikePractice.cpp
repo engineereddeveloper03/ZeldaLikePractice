82753a04fa72f497e0390b1500ba93adaaf3362d
Phillip <psturtevant400@gmail.com>
2024-01-31 15:16:04 -0800

    Initial Commit
    - Initial commit with new UE5 project

---
+++ Source/ZeldaLikePractice/ZeldaLikePractice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZeldaLikePractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZeldaLikePractice, "ZeldaLikePractice" );
