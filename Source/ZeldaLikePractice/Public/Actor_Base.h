23a598e6d51aad54b12f05d15e26c2dcbb4ec9c8
Phillip <psturtevant400@gmail.com>
2024-12-12 16:32:10 -0800

    Test Class Name Change
    - Testing changing the name of the class successfully

--- Source/ZeldaLikePractice/Public/Actor_Base.h
+++ Source/ZeldaLikePractice/Public/Actor_Base.h
@@ -4,16 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Object_Base.generated.h"
+#include "Actor_Base.generated.h"
 
 UCLASS()
-class ZELDALIKEPRACTICE_API AObject_Base : public AActor
+class ZELDALIKEPRACTICE_API AActor_Base : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	AObject_Base();
+	AActor_Base();
 
 protected:
 	// Called when the game starts or when spawned
